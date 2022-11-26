a82bde4f3e9747b8c1a3c53f70d9dddcd08a4a02
Samuel Näsström <nasstromsamuel@icloud.com>
2022-11-26 11:01:53 +0000

    added comment section

--- blog/admin.py
+++ blog/admin.py
@@ -1,25 +1,23 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Comment
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('status', 'created_on')
-    list_display = ('status', 'title', 'slug', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
-    summernote_fields = ('content')
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
-    search_fields = ['name', 'email', 'body']
+    search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
